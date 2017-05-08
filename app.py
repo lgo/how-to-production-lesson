ac23bfee3db10e8dfb328aefac8d4fa261133a40
Joey Pereira <joey@pereira.io>
2017-05-07 20:58:16 -0400

    opbeats

--- app.py
+++ app.py
@@ -12,6 +12,17 @@ bugsnag.configure(
 app = Flask(__name__)
 handle_exceptions(app)
 
+from opbeat.contrib.flask import Opbeat
+import time
+import random
+
+opbeat = Opbeat(
+    app,
+    organization_id='2b89a4ba846e4168badfff0cd7014ad0',
+    app_id='7c28c65c00',
+    secret_token='005b0c5022183aeacdafe45cd754e99e85c99558',
+)
+
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
@@ -20,5 +31,9 @@ def hello_world():
 def exception_endpoint():
     raise Exception("Uh oh, world")
 
+@app.route('/slow')
+def slow_endpoint():
+    time.sleep(random.randint(1,5))
+    return 'Zzzz, world!'
 
 app.run(host='0.0.0.0', port=5000)
