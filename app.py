7c68de9515a08a20e22fbf2d3dd351d19fba440c
Joey Pereira <joey@pereira.io>
2017-05-07 20:55:15 -0400

    yay

--- app.py
+++ app.py
@@ -1,5 +1,16 @@
 from flask import Flask
+
+import bugsnag
+from bugsnag.flask import handle_exceptions
+
+# Configure Bugsnag
+bugsnag.configure(
+  api_key = "c7dd55a21f726e33ff9658568b6aac42",
+  project_root = "/path/to/your/app",
+)
+
 app = Flask(__name__)
+handle_exceptions(app)
 
 @app.route('/')
 def hello_world():
@@ -8,5 +19,6 @@ def hello_world():
 @app.route('/exception')
 def exception_endpoint():
     raise Exception("Uh oh, world")
+
 
 app.run(host='0.0.0.0', port=5000)
